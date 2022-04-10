a856c0d2c905f5675dac6dbe4405aa9c4abfb282
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-04-10 14:11:27 +0800

    Create id_number.php
    Issue:
    The page throws err 500

---
+++ id_number.php
@@ -0,0 +1,112 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$db="demo";
+
+mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset([$_POST'idnumber'])){
+
+	$uname=$_POST['idnumber'];
+	$password=$_POST['password'];
+
+	$sql="select * from loginform where user='".$uname."'AND Pass='".$password."'
+	limit 1";
+
+	$result=mysql_query($sql);
+
+	if(mysql_num_rows($result)==1){
+		echo " you have login successfully";
+		exit();
+	}
+	else{
+		echo "You have entered wrong password";
+		exit();
+	}
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>AlumTrack | Login</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Roboto&display=swap" rel="stylesheet">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+	<!-- external css (make this last to let all API load)-->
+	<link rel="stylesheet" type="text/css" href="./style.css">
+
+</head>
+<body>
+		
+<div class="container-fluid">
+	<div class="row">
+		<div class="col">
+		<nav class="navbar navbar-dark navbar-expand-md bg-transparent sticky-top">
+     	   <a class="navbar-brand" href="./index.html">
+  		  <img src="./images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    		AlumTrack</a>
+		</nav>
+</div>
+		<div class="col">
+		<nav class="navbar-nav navbar-expand-md navbar-dark bg-transparent sticky-top p-2">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#portfolio-nav1" >
+				<span class=navbar-toggler-icon></span> <!-- toggler is hamburger menu -->
+			</button>
+    <div class="collapse navbar-collapse text-white" id="portfolio-nav1">
+        <ul class="navbar-nav ml-auto">
+            <li>
+                <a class="nav-link" href="#">Help</a>
+            </li>
+            <li>
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li>
+                <a class="nav-link" href="#">Login</a>
+            </li>
+        </ul>
+    </div>   
+		</nav>
+		</div>
+	</div>
+</div>
+
+	<div class="container-fluid text-white">
+		<section id="main" class="text-center align-middle bg-transparent mt-3">  <!-- change section id main -->
+			<div class="margin center mx-auto-md text-left"> <!-- left the words -->
+		<form method="POST" action="#">
+			<div class="form-group">
+				<label for="idnumber">ID number</label>
+				<input type="number" class="form-control" name="idnumber" placeholder="Enter ID number" min="1" max="9999999"><!-- issue: user can submit even the value is null -->			
+			</div>
+			<div class="form-group">
+				<label for="password">Passphrase</label>
+				<input type="password" name="password" class="form-control">
+				</div>
+				<div class="col-md-12 text-center">
+				<input type="submit" value="Login" class="btn btn-primary btn-size">
+				<a class="btn btn-primary btn-size" href="./onboarding.html" role="button">Debug</a>
+				</div> <!-- button label shouble be "NEXT" -->
+		</form>
+			
+		</section>
+</div>
+	</div>
+	</div>
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</body>
+</html>
