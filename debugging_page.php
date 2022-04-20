25aa9b057bf9225ba13c488ecc4e14de953242d2
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-04-20 14:21:05 +0800

    Create debugging_page.php
    this is page is  for testing the status of main_quest on php posting

---
+++ debugging_page.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<h2>Debugging Page </h2> <br>
+<br> Personal Information <br> <br>
+First name: <?php echo $_POST["first"]; ?><br>
+Middle initial: <?php echo $_POST["mi"]; ?> <br>
+Last name: <?php echo $_POST["last"]; ?> <br>
+Bday: <?php echo $_POST["datepicker"]; ?> <br>
+Gender: <?php echo $_POST["gender_selector"]; ?> <br>
+<br>
+Skills
+<br>
+Skill 1: <?php echo $_POST["skill_1"]; ?> <br> 
+Skill 2: <?php echo $_POST["skill_2"]; ?> <br>
+Skill 3: <?php echo $_POST["skill_3"]; ?> <br>
+Skill 4: <?php echo $_POST["skill_4"]; ?> <br>
+<br>
+Bio: <?php echo $_POST["bio"]; ?> <br>
+<br>
+Educational profile <br> <br>
+Current school: <?php echo $_POST["current_school"]; ?> <br>
+Current Educational Attainment: <?php echo $_POST["cur_educ_at"]; ?> <br>
+Course/Strand: <?php echo $_POST["cor_str"]; ?> <br>
+Specialization: <?php echo $_POST["specialization"]; ?> <br>
+Learning style: <?php echo $_POST["ls"]; ?> <br>
+Phone number: <?php echo $_POST["phone_num"]; ?> <br>
+<br>
+Work Profile
+<br> <br>
+Current work: <?php echo $_POST["current_work"]; ?> <br>
+
+</body>
+</html> 
+
